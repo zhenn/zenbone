ba752ff1b11715ae658261f52d6ac3604c00c85d
liying.chen <liying.chen@asiainnovations.com>
2016-12-21 11:15:59 +0800

    修改内容：1. zenbone component build添加 对demo的部署 2. 项目与组件脚手架修改，并添加README文件

--- component/temp/test.js
+++ component/temp/test.js
@@ -1,1 +1,45 @@
+import 'babel-polyfill';
+import './test.css';
 import './src/index';
+
+import React, {Component} from 'react';
+import ReactDOM, {render} from 'react-dom';
+
+class Text extends Component {
+
+    static defaultProps = {
+        p: '1'
+    };
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            q: 2
+        };
+        // ..
+    } 
+
+    render() {
+        return (
+            <div>
+                <h1>component/test</h1>
+                <div className="demo">
+                    <div className="demo-item">
+                        <h2>功能描述</h2>
+                        <div className="content">
+                            <button className="button-primary">功能</button>
+                            代码：
+                            <pre>
+                                <code>
+                                    import Test from 'c-test';
+                                </code>
+                            </pre>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+render(<Text />, $('#wrap')[0]); 
