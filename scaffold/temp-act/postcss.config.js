e3659bf3c7d1af13b59c18527bf46f825a93aa61
fqzhang <fuqiang.zhang@asiainnovations.com>
2018-03-15 18:46:33 +0800

    ci

--- scaffold/temp-act/postcss.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    plugins: [
-        // https://github.com/ai/browserslist
-        require('autoprefixer')({
-            browsers: ['last 100 versions']
-        })
-    ]
-};
